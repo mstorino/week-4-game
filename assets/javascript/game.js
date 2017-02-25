8cf3e2a1ce22b6fddefbe9716825e31414284cac
Maggie <test@apples-MacBook-Pro.local>
2017-02-25 09:29:31 -0600

    start index & css files

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+// 1. put value in .score
+// 2. give value to each jewel
+// 3. click jewel
+// 4. add value of jewel to the .totalScoreNumber
+// 5. continue to add value of jewel to .totalScoreNumber everytime it's clicked
+// 6. end game when the value of .totalScoreNumber equals the value of .score or value of .totalScoreNumber is greater than the value of .score
+// 7. if .totalScoreNumber equals value of .score say "You Win" in .scoreboard
+// 8. if .totalScoreNumber is greater than value of .score say "You lose" in .scoreboard
+// 9. if .totalScoreNumber equals value of .score add 1 to wins in .scoreboard
+// 10. if .totalScoreNumber equals value of .score add 1 to losses .scoreboard
+// 11. empty .totalScoreNumber
+
+
+
+
+
+// // variable list 
+// var objectHere = [
+//     "for",
+//     "the",
+//     "it",
+//     "up",
+//     "in",
+//     "she"
+// ]
+
+// var hangmanWord = "";
+
+
+
+// function startGame(){
+    
+// }
+
+
+
+// startGame();
